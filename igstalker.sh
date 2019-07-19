bbea8ca5f0b21fdcd4a76d936122a8d81e10f3fc
hax0r <39062772+widhisec@users.noreply.github.com>
2019-07-20 02:02:51 +0700

    Add files via upload

--- igstalker.sh
+++ igstalker.sh
@@ -18,37 +18,38 @@ clear;
 }
 search() {
 echo -n "cari :"; read az
-ngcari=$(curl -Gs "https://insta-stalker.com/search/?q=$az" --location-trusted \
+ngcari=$(curl -Gs "https://insta-stalkerr.com/search/?q=$az" --location-trusted \
 -H 'authority: insta-stalker.com' \
 -H 'upgrade-insecure-requests: 1' \
--H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86' | 
-grep "</span>" | cut -d ">" -f2 | cut -d "<" -f1  | cut -d "&" -f3)
-ngjuml=$(curl -Gs "https://insta-stalker.com/search/?q=$az" --location-trusted -H \
-'authority: insta-stalker.com' \
+-A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86' | 
+grep -Po '(?<=alt=").*?(?=")' | head | grep -Po "@(.*)" | grep -n ""
+)
+#printf "$ngcari \n" >> kamu
+printf "$ngcari \n"
+ngjuml=$(curl -Gs "https://insta-stalkerr.com/search/?q=$az" --location-trusted \
+-H 'authority: insta-stalkerr.com' \
 -H 'upgrade-insecure-requests: 1' \
--H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86' | 
-grep "</span>" | cut -d ">" -f2 | cut -d "<" -f1 | grep -n "" | grep -o "2:.*" | tr -d "2:" | gawk '{ print $1,$2 }'
+-A 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86' | 
+grep -Po '(?<=span).*?(?>")' | head -2 | tail -1 | gawk -F ">" '{print $2}' | gawk '{ print $1 }'
 )
 }
 ngclear
 getdata() {
-ngcur=$(curl -Gs "https://insta-stalker.com/profile/$us/" --location-trusted | 
-grep 'document.write' | grep ";</script><span>" |
-cut -d "." -f2 | grep ";</" |
-awk '/abbreviateNumber/{print $1}' | cut -d "b" -f4 | grep ";</script><span>Followers</span>" | grep -o "er(.*)" | tr -d "er())")
-ngcurl=$(curl -Gs "https://insta-stalker.com/profile/$us/" -L | 
-grep 'document.write' | grep ";</script><span>" |
-cut -d "." -f2 | grep ";</" |
-awk '/abbreviateNumber/{print $1}' | cut -d "b" -f4 | grep ";</script><span>Following</span>" | grep -o "er(.*)" | tr -d "er())")
-ngcurls=$(curl -Gs "https://insta-stalker.com/profile/$us/" -L | 
-grep 'document.write' | grep ";</script><span>" |
-cut -d "." -f2 | grep ";</" |
-awk '/abbreviateNumber/{print $1}' | cut -d "b" -f4 | grep ";</script><span>Posts</span>" | grep -o "er(.*)" | tr -d "er())")
+#echo -n "pilih:"; read yuser
+#ok=$(echo "$(cat kamu)"| grep -o "$yuser:.*" | cut -d ":" -f2)
+ngcur=$(curl -Gs "https://insta-stalkerr.com/profile/${you}/" --location-trusted | 
+grep -Po '(?<=div).*?(?>")' | grep "Followers" | cut -d ">" -f2 | grep -o ".*<" | tr -d "<")
+ngcurl=$(curl -Gs "https://insta-stalkerr.com/profile/${you}/" -L | 
+grep -Po '(?<=div).*?(?>")' | grep "Following" | cut -d ">" -f2 | grep -o ".*<" | tr -d "<")
+ngcurls=$(curl -Gs "https://insta-stalkerr.com/profile/${you}/" -L | 
+grep -Po '(?<=div).*?(?>")' | grep "Following" | cut -d ">" -f2 | grep -o ".*<" | tr -d "<")
 printf "[+] Followers: $ngcur\n"
 printf "[+] Following: $ngcurl\n"
 printf "[+] Posts: $ngcurls\n"
 }
 ngbanner
-search;printf "$ngcari";printf "found : $ngjuml\n"
-read -p "username:" us
-getdata $us
+search;
+echo -e "-----------------------------"
+printf "found : $ngjuml\n";
+echo -en "[*] cari :"; read you
+getdata $you
